78467ae55f418848d9186b5a823b2352326ad874
ZeynepGungor06 <zeynepgungor06@gmail.com>
2026-01-21 23:51:53 +0300

    jgyugyug
    nkjjknkjn

--- Assets/Scripts/core/LevelManager.cs
+++ Assets/Scripts/core/LevelManager.cs
@@ -7,18 +7,35 @@ public class LevelManager : MonoBehaviour
     public LevelData currentLevel;
     public TMP_Text orderText;
     private ProductData currentOrder;
+    public TMP_Text coinText;
+    private int coin = 0;
+
+public ProductData CurrentOrder => currentOrder;
 
     private void Start()
     {
         StartLevel();
     }
+    void UpdateCoinUI()
+{
+    if (coinText != null)
+        coinText.text = "Coin: " + coin;
+}
+public void AddCoin(int amount)
+{
+    coin += amount;
+    UpdateCoinUI();
+}
 
     public void StartLevel()
     {
         Debug.Log("LEVEL STARTED: " + (currentLevel != null ? currentLevel.levelIndex : -1));
+        
+         UpdateCoinUI();
          CreateNewOrder();
+        
     }
-   void CreateNewOrder()
+   public void CreateNewOrder()
 {
     if (currentLevel == null || currentLevel.newUnlockedProducts == null || currentLevel.newUnlockedProducts.Count == 0)
     {
@@ -49,4 +66,6 @@ public void TryServeProduct(ProductData served)
         Debug.Log("❌ Wrong product served!");
     }
 }
+
+
 }
